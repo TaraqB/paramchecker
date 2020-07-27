ad59d20c17d98df4d9cff0121fda278c0e526d54
TaraqB <68851196+TaraqB@users.noreply.github.com>
2020-07-27 16:52:21 +0530

    deleted x variable

--- dead-code.cpp
+++ dead-code.cpp
@@ -1,11 +1,8 @@
 class Point {
 private:
-    int x;
     int y;
 public:
-    Point() { this->x = 0; this->y=0; }
-    const inline int getX() { return this->x; }
+    Point() { this->y=0; }
     const inline int getY() { return this->y; }
-    inline void setX(int x) { this->x = x; }
     inline void setY(int y) { this->y = y; }
 };
