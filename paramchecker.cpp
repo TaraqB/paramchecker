b1011992bf0387c99d032226d99d17d8fa50a8fc
TaraqB <68851196+TaraqB@users.noreply.github.com>
2020-07-27 17:28:40 +0530

    Update paramchecker.cpp

--- paramchecker.cpp
+++ paramchecker.cpp
@@ -2,7 +2,7 @@
 bool vitalsAreOk(float bpm, float spo2, float respRate) {
   bool retValue = false;
   
- if(vitalSpo2Ok(bpm) & vitalBmpOk(spo2) & vitalrespRateOk(respRate))
+ if(vitalSpo2Ok(spo2) & vitalBmpOk(bpm) & vitalrespRateOk(respRate))
    retValue = true;
     
   return retValue;
@@ -11,6 +11,7 @@ bool vitalsAreOk(float bpm, float spo2, float respRate) {
 bool vitalBmpOk(float bpm)
 {
   bool retValue = true;
+  
   if(bpm < 70 || bpm > 150) {
     retValue =  false;
   }
@@ -20,6 +21,8 @@ bool vitalBmpOk(float bpm)
 
 bool vitalSpo2Ok(float spo2)
 {
+  bool retValue = true;
+  
   if(spo2 < 80) {
     retValue =  false;
   } 
